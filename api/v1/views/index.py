92fa1fc4f3ad23c52fffcd076c58556992f092c1
youssef-ait-elmahi <youssefaitelmahi3@gmail.com>
2023-10-29 17:06:59 +0100

    task 4 Create an endpoint

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,10 +1,30 @@
 #!/usr/bin/python3
 """running flask"""
 from api.v1.views import app_views
-from flask import jsonify
+from flask import jsonify, request
+from models import storage
+from models.state import State
+from models.city import City
+from models.user import User
+from models.place import Place
+from models.review import Review
+from models.amenity import Amenity
 
 
 @app_views.route('/status', methods=['GET'])
 def status():
     """return json status."""
     return jsonify({"status": "OK"})
+
+
+@app_views.route("/stats", methods=['GET'])
+def stats():
+    """return json status."""
+    return jsonify({
+            "amenities": storage.count(Amenity),
+            "cities": storage.count(City),
+            "places": storage.count(Place),
+            "reviews": storage.count(Review),
+            "states": storage.count(State),
+            "users": storage.count(User),
+        })
