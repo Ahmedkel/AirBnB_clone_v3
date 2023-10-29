93955d9de37ae5244a65275e23b71aba7b3f2ebd
youssef-ait-elmahi <youssefaitelmahi3@gmail.com>
2023-10-29 17:28:31 +0100

    task 5 create a handler for 404 errors

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,6 +2,7 @@
 """runing flask"""
 from api.v1.views import app_views
 from flask import Flask
+from flask import jsonify
 from models import storage
 import os
 
@@ -13,6 +14,12 @@ app.register_blueprint(app_views)
 def close_db(error):
     """Close storage on teardown."""
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """Handle 404 errors."""
+    return jsonify({"error": "Not found"}), 404
 
 
 if __name__ == "__main__":
